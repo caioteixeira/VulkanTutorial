fdeca29472f9557a1136c287a0b752f331a69732
Caio Vinicius Marques Teixeira <Caio Vinicius Marques Teixeira>
2017-02-05 22:01:48 -0200

    Physical devices

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -23,6 +23,15 @@ public:
 	}
 
 private:
+	struct QueueFamilyIndices {
+		int graphicsFamily = -1;
+
+		bool isComplete() {
+			return graphicsFamily >= 0;
+		}
+	};
+
+
 	GLFWwindow* window;
 	VDeleter<VkInstance> instance{ vkDestroyInstance };
 	VkDebugReportCallbackEXT callback;
@@ -214,7 +223,36 @@ private:
 	}
 
 	bool isDeviceSuitable(VkPhysicalDevice device) {
-		return true;
+		QueueFamilyIndices indices = findQueueFamilies(device);
+
+		return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		int i = 0;
+		for(const auto& queueFamily : queueFamilies)
+		{
+			if(queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.graphicsFamily = i;
+			}
+
+			if(indices.isComplete())
+			{
+				break;
+			}
+
+			i++;
+		}
+
+		return indices;
 	}
 
 	void mainLoop() {
